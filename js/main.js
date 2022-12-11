427a56e086f1fa133597e44811cf0bb379941a2f
Dmitry <103439106+Solmyrik@users.noreply.github.com>
2022-12-11 16:54:23 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,90 @@
+const iconMenu = document.querySelector('.header__burger');
+const menuBody = document.querySelector('.menu__body');
+if (iconMenu) {
+  iconMenu.addEventListener('click', function (e) {
+    document.body.classList.toggle('_lock');
+    iconMenu.classList.toggle('_active');
+    menuBody.classList.toggle('_active');
+  });
+}
+
+const menuItems = document.querySelectorAll('.menu__item');
+
+menuItems.forEach((e) => {
+  e.onclick = () => {
+    if (menuBody.classList.contains('_active')) {
+      iconMenu.classList.toggle('_active');
+      menuBody.classList.toggle('_active');
+      document.body.classList.toggle('_lock');
+    }
+  };
+});
+
+function ibg() {
+  let ibg = document.querySelectorAll('.ibg');
+  for (var i = 0; i < ibg.length; i++) {
+    if (ibg[i].querySelector('img')) {
+      ibg[i].style.backgroundImage = 'url(' + ibg[i].querySelector('img').getAttribute('src') + ')';
+    }
+  }
+}
+ibg();
+
+new Swiper('.slider', {
+  slidesPerView: 1,
+  loop: true,
+  speed: 800,
+  navigation: {
+    nextEl: '.arrow_next',
+    prevEl: '.arrow_prev',
+  },
+});
+
+const mapSection = document.querySelector('.adres__map');
+const mapSectionTwo = document.querySelector('.adres__map_two');
+if (mapSection) {
+  ymaps.ready(init);
+
+  function init() {
+    let map = new ymaps.Map('map1', {
+      center: [64.54208212885155, 40.52495164418026],
+      zoom: 17,
+    });
+
+    let placemark = new ymaps.Placemark([64.54208212885155, 40.52495164418026], {}, {});
+
+    map.controls.remove('geolocationControl');
+    map.controls.remove('searchControl');
+    map.controls.remove('trafficControl');
+    map.controls.remove('typeSelector');
+    map.controls.remove('fullscreenControl');
+    map.controls.remove('zoomControl');
+    map.controls.remove('rulerControl');
+    map.behaviors.disable(['scrollZoom']);
+
+    map.geoObjects.add(placemark);
+  }
+}
+if (mapSectionTwo) {
+  ymaps.ready(init);
+
+  function init() {
+    let map = new ymaps.Map('map2', {
+      center: [64.5454982140776, 40.531745230653726],
+      zoom: 17,
+    });
+
+    let placemark = new ymaps.Placemark([64.5454982140776, 40.531745230653726], {}, {});
+
+    map.controls.remove('geolocationControl');
+    map.controls.remove('searchControl');
+    map.controls.remove('trafficControl');
+    map.controls.remove('typeSelector');
+    map.controls.remove('fullscreenControl');
+    map.controls.remove('zoomControl');
+    map.controls.remove('rulerControl');
+    map.behaviors.disable(['scrollZoom']);
+
+    map.geoObjects.add(placemark);
+  }
+}
